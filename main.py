c08dc7631f35e2ba2be68560b76d76decfd7f28b
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-16 15:36:07 +1000

    download book covers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit, unquote
 
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 import requests
 
 
@@ -14,25 +15,38 @@ def download_txt(url, filename, folder='books/'):
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
 
-    try:
-        check_for_redirect(response)
-        filepath = os.path.join(folder, sanitize_filename(filename))
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        with open(filepath, 'wb') as file:
-            file.write(response.content)
+    check_for_redirect(response)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
-        return filepath
-    except requests.exceptions.HTTPError:
-        print(f'Book with with link {url} does not exist')
+    return filepath
+
+
+def download_image(url, filename, folder='images/'):
+    return download_txt(url, filename, folder)
 
 
 if __name__ == '__main__':
-    for book_id in range(1, 11):
-        response = requests.get(f'https://tululu.org/b{book_id}')
-        soup = BeautifulSoup(response.text, 'lxml')
-        title = soup.find('h1').text.split('::')[0].strip()
+        for book_id in range(1, 11):
+            try:
+                url = f'https://tululu.org/b{book_id}/'
+                response = requests.get(url, allow_redirects=False)
+                check_for_redirect(response)
+                soup = BeautifulSoup(response.text, 'lxml')
+                title = soup.find('h1').text.split('::')[0].strip()
 
-        filepath = download_txt(f'https://tululu.org/txt.php?id={book_id}', f'{book_id}. {title}.txt')
+                img = soup.find(class_='bookimage').find('img').attrs.get('src')
+                img_url = urljoin('https://tululu.org', img)
 
-        if (filepath):
-            print(f'{filepath} downloaded')
+                book_filepath = download_txt(f'https://tululu.org/txt.php?id={book_id}', f'{book_id}. {title}.txt')
+                img_filepath = download_image(img_url, unquote(img).split('/')[-1])
+
+                if (book_filepath):
+                    print(f'{book_filepath} downloaded')
+
+                if (img_filepath):
+                    print(f'{img_filepath} downloaded')
+            except requests.exceptions.HTTPError:
+                print(f'Book with with link {url} does not exist')
