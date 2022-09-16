7a655fa2db09c6e8d91aa7a33df18ef90ffa65cf
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-16 16:58:57 +1000

    move book parsing code to parse_book_page function

--- main.py
+++ main.py
@@ -29,20 +29,33 @@ def download_image(url, filename, folder='images/'):
     return download_txt(url, filename, folder)
 
 
+def parse_book_page(page):
+    soup = BeautifulSoup(page, 'lxml')
+    title, author = [item.strip() for item in soup.find('h1').text.split('::')]
+    comments = [comment.text for comment in soup.select('.texts .black')]
+    genres = [genre.text for genre in soup.select_one('.d_book:-soup-contains("Жанр книги:")').find_all('a')]
+    img = soup.find(class_='bookimage').find('img').attrs.get('src')
+
+    return {
+        'title': title,
+        'author': author,
+        'comments': comments,
+        'genres': genres,
+        'img': img
+    }
+
+
 if __name__ == '__main__':
         for book_id in range(1, 11):
             try:
                 url = f'https://tululu.org/b{book_id}/'
                 response = requests.get(url, allow_redirects=False)
                 check_for_redirect(response)
-                soup = BeautifulSoup(response.text, 'lxml')
-                title = soup.find('h1').text.split('::')[0].strip()
-                comments = soup.select('.texts .black')
-                genres = soup.select_one('.d_book:-soup-contains("Жанр книги:")').find_all('a')
-                print([comment.text for comment in comments])
-                print([genre.text for genre in genres])
+                book_info = parse_book_page(response.text)
+                title = book_info['title']
+                img = book_info['img']
+                
 
-                img = soup.find(class_='bookimage').find('img').attrs.get('src')
                 img_url = urljoin('https://tululu.org', img)
 
                 book_filepath = download_txt(f'https://tululu.org/txt.php?id={book_id}', f'{book_id}. {title}.txt')
